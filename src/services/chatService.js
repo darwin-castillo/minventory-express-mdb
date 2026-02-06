1acd002c11194e1c9f7eda7575cf5f4fc9c5dbed
Darwin <darwin.c5@gmail.com>
2026-02-06 12:26:04 -0400

    fix google ai dependency

--- src/services/chatService.js
+++ src/services/chatService.js
@@ -1,8 +1,8 @@
 // src/services/chatService.js
-const { GoogleGenerativeAI } = require("@google/generative-ai");
+const { GoogleGenAI } = require("@google/genai");
 const productService = require("./productService");
 
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const ai = new GoogleGenAI({});
 
 const askChatbot = async (userQuestion) => {
   // 1. Buscamos productos relacionados en la base de datos
@@ -13,8 +13,6 @@ const askChatbot = async (userQuestion) => {
     `- Producto: ${p.name}, Precio: ${p.price}, Stock: ${p.stock}, Categoría: ${p.category}`
   ).join("\n");
 
-  // 3. Generamos la respuesta con Gemini
-  const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash", });
 
   const prompt = `
     Actúa como un asistente de ventas de tecnología experto y amable.
@@ -28,9 +26,14 @@ const askChatbot = async (userQuestion) => {
     - Si el stock es bajo (menos de 3), advierte al cliente.
     - Si no encuentras nada, dile que contacte a Claudia Hoyos (+584143381704) para pedidos especiales.
   `;
+  // 3. Generamos la respuesta con Gemini
+  const result = await ai.models.generateContent({ model: "gemini-2.5-flash", contents: prompt });
 
-  const result = await model.generateContent(prompt);
-  return result.response.text();
+
+
+  //const result = await model.generateContent(prompt);
+  // return result.response.text();
+  return result.text;
 };
 
 module.exports = { askChatbot };
